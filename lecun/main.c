2701f97b02f14cf3c0035cbeea6b32119f92cc35
Jan Grzybek <lyre_embassy_0n@icloud.com>
2024-03-26 02:13:14 +0000

    refactor

--- lecun/main.c
+++ lecun/main.c
@@ -55,11 +55,8 @@ typedef struct Images {
 } Images;
 
 unsigned long images_allocate(Images* images) {
-    print_maybe("Success");
     unsigned long memory_needed = images->count * images->width * images->height * sizeof(unsigned char);
-    print_maybe("Success");
     images->data = malloc(memory_needed);
-    print_maybe("Success");
     return memory_needed;
 }
 
@@ -67,7 +64,66 @@ void images_deallocate(Images* images) {
     free(images->data);
 }
 
-Images rescale(Images images) {
+void show_image(Images images, int index) {
+    int img_offset = index * images.height * images.width;
+    print_maybe("Image %d, %d x %d\n", index, images.height, images.width);
+    for (int col = 0; col < images.width+2; col++) print_maybe("# #"); print_maybe("\n");
+    for (int row = 0; row < images.height; row++) {
+        print_maybe("#  ");
+        int row_offset = img_offset + row * images.width;
+        for (int col = 0; col < images.width; col++) {
+            int idx = row_offset + col;
+            if (images.data[idx] > 200) print_maybe("@@@");
+            else if (images.data[idx] > 100) print_maybe("111");
+            else if (images.data[idx] > 50) print_maybe("...");
+            else if (images.data[idx] > 25) print_maybe(" . ");
+            else print_maybe("   ");
+        }
+        print_maybe("  #");
+        print_maybe("\n");
+    }
+    for (int col = 0; col < images.width+2; col++) print_maybe("# #"); print_maybe("\n");
+}
+
+struct BilinearGrid {
+    double* target_centers;
+    double* ratios_first;
+    double* ratios_second;
+    int* indices_first;
+    int* indices_second;
+};
+
+void grid_allocate(struct BilinearGrid* grid, int target_size) {
+    unsigned long memory_needed_double = target_size * sizeof(double);
+    unsigned long memory_needed_int = target_size * sizeof(int);
+    grid->target_centers = malloc(memory_needed_double);
+    grid->ratios_first = malloc(memory_needed_double);
+    grid->ratios_second = malloc(memory_needed_double);
+    grid->indices_first = malloc(memory_needed_int);
+    grid->indices_second = malloc(memory_needed_int);
+}
+
+void grid_deallocate(struct BilinearGrid* grid) {
+    free(grid->target_centers);
+    free(grid->ratios_first);
+    free(grid->ratios_second);
+    free(grid->indices_first);
+    free(grid->indices_second);
+}
+
+void grid_calculate(struct BilinearGrid* grid, int original_size, int target_size) {
+    for (int i = 0; i < target_size; i++) {
+        grid->target_centers[i] = (2 * (double)i + 1) * (double)original_size / (2 * (double)target_size);
+        grid->indices_first[i] = (int)(grid->target_centers[i] - 0.5);
+        int x1 = (int)(grid->target_centers[i] + 0.5);
+        grid->ratios_first[i] = x1 - grid->target_centers[i] + 0.5;
+        grid->ratios_second[i] = grid->target_centers[i] - (grid->indices_first[i] + 0.5);
+        if (x1 < original_size) grid->indices_second[i] = x1;
+        else grid->indices_second[i] = original_size - 1;
+    }
+}
+
+Images resize_bilinear(Images images) {
     Images rescaled_images;
     rescaled_images.magic_number = images.magic_number;
     rescaled_images.count = images.count;
@@ -75,101 +131,43 @@ Images rescale(Images images) {
     rescaled_images.height = 16;
     images_allocate(&rescaled_images);
 
+    struct BilinearGrid horizontal_grid;
+    grid_allocate(&horizontal_grid, rescaled_images.width);
+    grid_calculate(&horizontal_grid, images.width, rescaled_images.width);
+    struct BilinearGrid vertical_grid;
+    grid_allocate(&vertical_grid, rescaled_images.height);
+    grid_calculate(&vertical_grid, images.height, rescaled_images.height);
+
     int original_resolution = images.height * images.width;
-    if (is_verbose()) {
-        for (int img = 0; img < images.count; img++) {
-            bool do_print = img % 10000 == 0;
-            if (do_print) print_maybe("Image %d\n", img);
-            else continue;
-            for (int row = 0; row < images.height; row++) {
-                for (int col = 0; col < images.width; col++) {
-                    int idx = img * original_resolution + row * images.width + col;
-                    if (images.data[idx] > 200) print_maybe("@@@");
-                    else if (images.data[idx] > 100) print_maybe("111");
-                    else if (images.data[idx] > 50) print_maybe("...");
-                    else if (images.data[idx] > 25) print_maybe(" . ");
-                    else print_maybe("   ");
-                }
-                print_maybe("\n");
-            }
-            print_maybe("\n");
-        }
-    }
-
-    double xt_centers[rescaled_images.width];
-    double x0_ratios[rescaled_images.width];
-    double x1_ratios[rescaled_images.width];
-    int x0_indices[rescaled_images.width];
-    int x1_indices[rescaled_images.width];
-    double yt_centers[rescaled_images.height];
-    double y0_ratios[rescaled_images.height];
-    double y1_ratios[rescaled_images.height];
-    int y0_indices[rescaled_images.height];
-    int y1_indices[rescaled_images.height];
-    for (int i = 0; i < rescaled_images.width; i++) {
-        xt_centers[i] = (2 * (double)i + 1) * (double)images.width / (2 * (double)rescaled_images.width);
-        x0_indices[i] = (int)(xt_centers[i] - 0.5);
-        int x1 = (int)(xt_centers[i] + 0.5);
-        x0_ratios[i] = x1 - xt_centers[i] + 0.5;
-        x1_ratios[i] = xt_centers[i] - (x0_indices[i] + 0.5);
-        if (x1 < images.width) x1_indices[i] = x1;
-        else x1_indices[i] = images.width - 1;
-    }
-    for (int i = 0; i < rescaled_images.height; i++) {
-        yt_centers[i] = (2 * (double)i + 1) * (double)images.height / (2 * (double)rescaled_images.height);
-        y0_indices[i] = (int)(yt_centers[i] - 0.5);
-        int y1 = (int)(yt_centers[i] + 0.5);
-        y0_ratios[i] = y1 - yt_centers[i] + 0.5;
-        y1_ratios[i] = yt_centers[i] - (y0_indices[i] + 0.5);
-        if (y1 < images.height) y1_indices[i] = y1;
-        else y1_indices[i] = images.height - 1;
-    }
-
-//    for (int i = 0; i < rescaled_images.width; i++) {
-//        printf("------\n");
-//        printf("%f\n", xt_centers[i]);
-//        printf("%f\n", x0_ratios[i]);
-//        printf("%f\n", x1_ratios[i]);
-//        printf("%d\n", x0_indices[i]);
-//        printf("%d\n", x1_indices[i]);
-//    }
-
     int target_resolution = rescaled_images.height * rescaled_images.width;
     for (int img = 0; img < rescaled_images.count; img++) {
-        int idx0 =  img * target_resolution;
-        int img_offset = img * original_resolution;
+        int t_img_offset =  img * target_resolution;
+        int o_img_offset = img * original_resolution;
         for (int row = 0; row < rescaled_images.height; row++) {
-            int idx1 = idx0 + row * rescaled_images.width;
-            int y0_offset = y0_indices[row] * images.width + img_offset;
-            int y1_offset = y1_indices[row] * images.width + img_offset;
+            int idx1 = t_img_offset + row * rescaled_images.width;
+            int y0_offset = vertical_grid.indices_first[row] * images.width + o_img_offset;
+            int y1_offset = vertical_grid.indices_second[row] * images.width + o_img_offset;
             for (int col = 0; col < rescaled_images.width; col++) {
-                double a = x0_ratios[col]*images.data[x0_indices[col]+y0_offset];
-                double b = x1_ratios[col]*images.data[x1_indices[col]+y0_offset];
-                double c = x0_ratios[col]*images.data[x0_indices[col]+y1_offset];
-                double d = x1_ratios[col]*images.data[x1_indices[col]+y1_offset];
-                rescaled_images.data[idx1 + col] = (unsigned char)(y0_ratios[row] * (a+b) + y1_ratios[row] * (c+d));
+                double a = horizontal_grid.ratios_first[col] *
+                        images.data[horizontal_grid.indices_first[col]+y0_offset];
+                double b = horizontal_grid.ratios_second[col] *
+                        images.data[horizontal_grid.indices_second[col]+y0_offset];
+                double c = horizontal_grid.ratios_first[col] *
+                        images.data[horizontal_grid.indices_first[col]+y1_offset];
+                double d = horizontal_grid.ratios_second[col] *
+                        images.data[horizontal_grid.indices_second[col]+y1_offset];
+                rescaled_images.data[idx1 + col] = (unsigned char)(vertical_grid.ratios_first[row] * (a+b) +
+                        vertical_grid.ratios_second[row] * (c+d));
             }
         }
     }
 
+    grid_deallocate(&horizontal_grid);
+    grid_deallocate(&vertical_grid);
+
     if (is_verbose()) {
-        for (int img = 0; img < rescaled_images.count; img++) {
-            bool do_print = img % 10000 == 0;
-            if (do_print) print_maybe("Image %d\n", img);
-            else continue;
-            for (int row = 0; row < rescaled_images.height; row++) {
-                for (int col = 0; col < rescaled_images.width; col++) {
-                    int idx = img * target_resolution + row * rescaled_images.width + col;
-                    if (rescaled_images.data[idx] > 200) print_maybe("@@@");
-                    else if (rescaled_images.data[idx] > 100) print_maybe("111");
-                    else if (rescaled_images.data[idx] > 50) print_maybe("...");
-                    else if (rescaled_images.data[idx] > 25) print_maybe(" . ");
-                    else print_maybe("   ");
-                }
-                print_maybe("\n");
-            }
-            print_maybe("\n");
-        }
+        show_image(images, 47);
+        show_image(rescaled_images, 47);
     }
 
     images_deallocate(&images);
@@ -195,16 +193,16 @@ Images load_mnist() {
     int offset = 4 * sizeof(int32_t);
     fseek(fptr, offset, SEEK_SET);
 
-    print_maybe("Loading %d images ...\n", images.count);
+    print_maybe("Loading %d images ... ", images.count);
     unsigned long memory_needed = images_allocate(&images);
     fread(images.data, memory_needed, 1, fptr);
 
     fclose(fptr);
-    print_maybe("Success.");
+    print_maybe("success.\n");
     return images;
 }
 
 int main() {
-    rescale(load_mnist());
+    resize_bilinear(load_mnist());
     return 0;
 }
